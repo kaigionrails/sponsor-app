112f22301b8ccc18f91a8a4613dd040f49e740be
Yusuke Nakamura <yusuke1994525@gmail.com>
2024-07-16 22:25:54 +0900

    We have no attendee tickets for sponsors

--- app/jobs/sponsorship_welcome_job.rb
+++ app/jobs/sponsorship_welcome_job.rb
@@ -15,6 +15,6 @@ class SponsorshipWelcomeJob < ApplicationJob
       { text: ":tamago: *New sponsorship* (#{sponsorship.plan_name || '*OTHER*'}): #{sponsorship.name}  <#{conference_sponsorship_url(sponsorship.conference, sponsorship)}|Open>" },
     )
 
-    EnsureSponsorshipTitoDiscountCodeJob.perform_later(sponsorship, 'attendee')
+    # EnsureSponsorshipTitoDiscountCodeJob.perform_later(sponsorship, 'attendee')
   end
 end
