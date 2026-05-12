defe8a1a36741b53b1da60fe0b7079895d328905
Yusuke Nakamura <unasuke@users.noreply.github.com>
2026-05-12 12:50:57 +0900

    Revert "Restore SponsorshipAssetFile#object_key method"

--- app/models/sponsorship_asset_file.rb
+++ app/models/sponsorship_asset_file.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class SponsorshipAssetFile < ApplicationRecord
-  PREFIX = ENV['S3_FILES_PREFIX']
-
   include AssetFileUploadable
 
   ALLOWED_CONTENT_TYPES = %w[
@@ -41,11 +39,6 @@ class SponsorshipAssetFile < ApplicationRecord
     )
     dst.update_object_header
     dst
-  end
-
-  def object_key
-    raise unless self.persisted?
-    "#{PREFIX}#{prefix}#{handle}--#{id}.#{extension}"
   end
 
   def filename
