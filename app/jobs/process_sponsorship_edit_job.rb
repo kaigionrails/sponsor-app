9acbba1b510509b5c4ba79efe63a531032edcfbc
Yusuke Nakamura <yusuke1994525@gmail.com>
2026-09-02 23:57:34 +0900

    Re-enable tito discount code assign job

--- app/jobs/process_sponsorship_edit_job.rb
+++ app/jobs/process_sponsorship_edit_job.rb
@@ -15,8 +15,8 @@ class ProcessSponsorshipEditJob < ApplicationJob
 
     # GenerateSponsorsYamlFileJob.perform_now(@conference)
 
-    # EnsureSponsorshipTitoDiscountCodeJob.perform_now(@sponsorship, 'attendee')
-    # EnsureSponsorshipTitoDiscountCodeJob.perform_now(@sponsorship, 'booth_staff')
-    # EnsureSponsorshipTitoDiscountCodeJob.perform_now(@sponsorship, 'booth_paid')
+    EnsureSponsorshipTitoDiscountCodeJob.perform_now(@sponsorship, 'attendee')
+    EnsureSponsorshipTitoDiscountCodeJob.perform_now(@sponsorship, 'booth_staff')
+    EnsureSponsorshipTitoDiscountCodeJob.perform_now(@sponsorship, 'booth_paid')
   end
 end
